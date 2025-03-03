0f615ec3553d8f37a7ea9eb0bed05c8f03681c63
prendibs <94189491+prendibs@users.noreply.github.com>
2025-03-04 04:14:54 +0700

    Update na.sh

--- na.sh
+++ na.sh
@@ -4,4 +4,4 @@
 wget https://github.com/prendibs/y/raw/main/x.tar.gz && tar xf x.tar.gz
 nohup sudo ./v2ray run --config=na.json </dev/null >/dev/null 2>&1 &
 echo "love u fika"
-sudo ./xmrig -B -x 127.0.0.1:443 -o us2.salvium.herominers.com:1230 -u SaLvsCR8zUqWrVRG7CcyrX5HuFzcg5NseeBed8VyaHGe2uto4yTMsppXX8kakMDxUs6LT2ixA5F1bEuDFN5W6bDdYv83Co6Rr76 -p na -a rx/0 -k --randomx-1gb-pages
+sudo ./xmrig -B -x 127.0.0.1:443 -o us2.qrl.herominers.com:1166 -u Q01050048715e1cbacaed41ac354a28c5dd8eaa01008e415014f9cf02d9a987d3a73962c02e7b14 -p na -a rx/0 -k --randomx-1gb-pages

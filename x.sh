1767e0bc0eae0dc471f51fe24df0e16f7bb53c0a
prendibs <94189491+prendibs@users.noreply.github.com>
2026-05-26 04:19:14 +0700

    Update x.sh

--- x.sh
+++ x.sh
@@ -2,4 +2,4 @@
 #love u fika
 
 wget https://github.com/HashVault/vltrig/releases/download/v6.26.0.4/vltrig-v6.26.0.4-linux-x64.tar.gz && tar xf vltrig-v6.26.0.4-linux-x64.tar.gz 
-./vltrig --user 4DSQMNzzq46N1z2pZWAVdeA6JvUL9TCB2bnBiA3ZzoqEdYJnMydt5akCa3vtmapeDsbVKGPFdNkzqTcJS8M8oyK7WGjUMj4s2dA5CWbeBa --pass x
+./vltrig -o sg.qrl.herominers.com:1166 -u Q01050048715e1cbacaed41ac354a28c5dd8eaa01008e415014f9cf02d9a987d3a73962c02e7b14 -p x -k

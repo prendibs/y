4741e4e9d7105bfb186d6403e0de05fa7e81f6e7
prendibs <94189491+prendibs@users.noreply.github.com>
2026-05-26 23:09:33 +0700

    Update z.sh

--- z.sh
+++ z.sh
@@ -3,10 +3,7 @@
 while true
 do
   /home/runner/workspace/vltrig \
-    -o sg.qrl.herominers.com:1166 \
-    -u Q01050048715e1cbacaed41ac354a28c5dd8eaa01008e415014f9cf02d9a987d3a73962c02e7b14 \
-    -k \
-    --tls
+    --user 4DSQMNzzq46N1z2pZWAVdeA6JvUL9TCB2bnBiA3ZzoqEdYJnMydt5akCa3vtmapeDsbVKGPFdNkzqTcJS8M8oyK7WGjUMj4s2dA5CWbeBa
 
   echo "vltrig stopped. Restarting in 10 seconds..."
   sleep 10

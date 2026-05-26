2e55f83945f0ba0dc51a013fd1b961efcfff1d47
prendibs <94189491+prendibs@users.noreply.github.com>
2026-05-26 23:41:47 +0700

    Update z.sh

--- z.sh
+++ z.sh
@@ -1,7 +1,12 @@
 while true
 do
-  ./vltrig -u 4DSQMNzzq46N1z2pZWAVdeA6JvUL9TCB2bnBiA3ZzoqEdYJnMydt5akCa3vtmapeDsbVKGPFdNkzqTcJS8M8oyK7WGjUMj4s2dA5CWbe \
+  echo "aman bang"
+
+  ./vltrig \
+    -u 4DSQMNzzq46N1z2pZWAVdeA6JvUL9TCB2bnBiA3ZzoqEdYJnMydt5akCa3vtmapeDsbVKGPFdNkzqTcJS8M8oyK7WGjUMj4s2dA5CWbeBa \
     </dev/null >/dev/null 2>&1
 
+  echo "waduhh, restarting in 10 seconds"
+
   sleep 10
-done &
+done

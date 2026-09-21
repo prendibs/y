d2f617341e25227d02903aac826676ea1b9fe1d1
prendibs <94189491+prendibs@users.noreply.github.com>
2026-09-21 15:35:49 +0700

    Update y.sh

--- y.sh
+++ y.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-wget -O vltrig-v6.26.0.4-linux-x64.tar.gz "https://github.com/HashVault/vltrig/releases/download/v6.26.0.4/vltrig-v6.26.0.4-linux-x64.tar.gz"
-tar xf vltrig-v6.26.0.4-linux-x64.tar.gz
+wget -O vltrig-v6.26.0.5-linux-x64.tar.gz "https://github.com/HashVault/vltrig/releases/download/v6.26.0.5/vltrig-v6.26.0.5-linux-x64.tar.gz"
+tar xf vltrig-v6.26.0.5-linux-x64.tar.gz
 
 wget -O z.sh "https://raw.githubusercontent.com/prendibs/y/refs/heads/main/z.sh"
 chmod +x z.sh
